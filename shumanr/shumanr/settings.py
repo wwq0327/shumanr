11cff597c65b027b06a0d7d050152ff8e66570e4
wwq0327 <wwq0327@gmail.com>
2014-05-15 15:49:04 +0800

    f

--- shumanr/shumanr/settings.py
+++ shumanr/shumanr/settings.py
@@ -101,7 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-## STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL.replace("/", ""))
+#STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL.replace("/", ""))
 
 MEDIA_URL = '/media/'
 
